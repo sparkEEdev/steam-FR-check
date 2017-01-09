94ececf9c3e8b6f6604c33d61826700a7e4b74e2
Sasa Marjanovic <sparkeeftw@gmail.com>
2017-01-09 19:48:43 +0100

    remove steam-tradeoffer-manager module, update readme

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-/*
-const TradeOfferManager = require('steam-tradeoffer-manager');*/
+
 const SteamUser = require('steam-user');
 const SteamCommunity = require('steamcommunity');
 const SteamTotp = require('steam-totp');
@@ -9,18 +8,12 @@ const check = require('./checker').friendsCheck;
 const cfg = require('./config_parser');
 
 // some settings
-var currency = 3; // 3 = euro (see: https://github.com/SteamRE/SteamKit/blob/master/Resources/SteamLanguage/enums.steamd#L696)
 var appid = 730; // 730 = CS:GO, 570 = DotA2, 440 = TF2
-var priceHist = JSON.parse(fs.readFileSync('itemHistory.json')); 
-var pendingReq = [];
+var priceHist = JSON.parse(fs.readFileSync('itemHistory.json')); // file from which we gather price history for our process: https://github.com/sparkEEgit/steam-item-appraiser << 
+var pendingReq = []; // array of friend requests(steamID64)
 
 var accountTradeHandler = function (username, password, sharedSecret) {
     var client = new SteamUser();
-    /*var manager = new TradeOfferManager({
-        "steam": client,
-        "domain": "somedomain.com",
-        "language": "en"
-    });*/
     var community = new SteamCommunity();
 
     client.logOn({
@@ -36,17 +29,8 @@ var accountTradeHandler = function (username, password, sharedSecret) {
     });
 
     client.on('webSession', function (sessionID, cookies) {
-        /*manager.setCookies(cookies, function (err) {
-            if (err) {
-                console.log(err);
-                process.exit(1);
-                return;
-            }
-        });*/
-
         community.setCookies(cookies);
         community.startConfirmationChecker(50000, "identitySecret" + username);
-
     });
 
     client.on('friendRelationship', function (steamID, relationship) {
@@ -54,17 +38,16 @@ var accountTradeHandler = function (username, password, sharedSecret) {
             pendingReq.push(steamID.getSteamID64());
             console.log("New friend request, ID: " + steamID.getSteamID64());
             check(community, this, pendingReq, priceHist);
-        }
-
+        };
     });
 
     client.on("friendsList", function () {
         for (var val in obj = this.myFriends) {
             if (obj[val] == 2) pendingReq.push(val);
-        }
+        };
         check(community, this, pendingReq, priceHist);
-    })
-}
+    });
+};
 
 for (i = 0; i < cfg.accountLoginInfos.length; i++) {
     accountTradeHandler(cfg.accountLoginInfos[i][0], cfg.accountLoginInfos[i][1], cfg.accountLoginInfos[i][2]);
